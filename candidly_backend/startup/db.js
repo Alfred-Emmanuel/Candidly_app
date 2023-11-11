bcd7c31854f97544b07a0212691a4903dc663539
Alfred Emmanuel <ealfred828@gmail.com>
2023-11-11 14:16:13 +0100

    heroku commit

--- candidly_backend/startup/db.js
+++ candidly_backend/startup/db.js
@@ -2,7 +2,9 @@ const winston = require("winston");
 const mongoose = require("mongoose");
 
 module.exports = function () {
+  // mongoose
+  //   .connect("mongodb://localhost:27017/candidly")
   mongoose
-    .connect("mongodb://localhost:27017/candidly")
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => winston.info("Connected to MongoDB..."))
 };
