baff0963c746054153d1e5b6b78272ce93de22cb
Alfred Emmanuel <ealfred828@gmail.com>
2023-11-13 18:29:30 +0100

    made the reset password and forgot password pages responsive

--- candidly_frontend/my-app/app/reset_password/[[...reset]]/page.jsx
+++ candidly_frontend/my-app/app/reset_password/[[...reset]]/page.jsx
@@ -3,9 +3,9 @@ import { NewPasswordForm } from "@/components/NewPasswordForm";
 
 const page = ({params}) => {
   return (
-    <div className="md:flex items-center justify-center border h-screen md:pt-[3%] md:pb-[3%] ">
-      <div className="md:w-[30%] border border-t-[#000080] border-t-8 rounded-lg max-h-[90%] mx-auto md:py-5 md:px-5 shadow-md">
-        <h1 className="text-[1.35rem] font-semibold md:mb-10">
+    <div className="flex items-center justify-center border h-screen md:pt-[3%] md:pb-[3%] ">
+      <div className="md:w-[30%] border border-t-[#000080] border-t-8 rounded-lg max-h-[90%] mx-auto py-5 px-5 shadow-md">
+        <h1 className="md:text-[1.35rem] font-semibold md:mb-10">
           Reset Your Password Below
         </h1>
         <div>
